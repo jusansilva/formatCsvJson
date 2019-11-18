2c5105eb136e173d1400e8890f5e27e29fb8c1aa
Jusan Magno <jusanmagno@gmail.com>
2019-11-18 18:51:29 +0000

    função de conversão

--- index.js
+++ index.js
@@ -2,9 +2,62 @@ const express = require('express')
 const app = new express()
 
 app.get('/', function(req, res){
-    res.send("app conectado");
+    res.sendFile(__dirname + "/views/index.html");
 });
 
-app.listen(3000, function(){
+app.get('/forJson', function(req, res){
+    
+});
+
+app.get('/forCsv', function(req, res){
+
+});
+
+
+function JsonToCSV(objArray) {
+    var array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
+    var str = '';
+
+    for (var i = 0; i < array.length; i++) {
+        var line = '';
+        for (var index in array[i]) {
+            if (line != '') line += ','
+
+            line += array[i][index];
+        }
+
+        str += line + '\r\n';
+    }
+
+    return str;
+}
+
+function csvToJSON(csv){
+
+  var lines=csv.split("\n");
+
+  var result = [];
+
+  var headers=lines[0].split(",");
+
+  for(var i=1;i<lines.length;i++){
+
+	  var obj = {};
+	  var currentline=lines[i].split(",");
+
+	  for(var j=0;j<headers.length;j++){
+		  obj[headers[j]] = currentline[j];
+	  }
+
+	  result.push(obj);
+
+  }
+  
+  //return result; //JavaScript object
+  return JSON.stringify(result); //JSON
+}
+
+
+app.listen(5000, function(){
     console.log('servidor conectado')
 })
